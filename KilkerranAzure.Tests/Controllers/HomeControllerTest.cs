e23a0f748caf313797764f15f8e4a7bfa30259fe
unknown <jobor@WIN7SEIT4013.hm.com>
2013-05-02 15:21:15 +0200

    Initial Commit

---
+++ KilkerranAzure.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+using KilkerranAzure.Controllers;
+
+using NUnit.Framework;
+
+namespace KilkerranAzure.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult viewResult = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(viewResult);
+        }
+    }
+}
---
+++ KilkerranAzure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KilkerranAzure.MvcApplication" Language="C#" %>
